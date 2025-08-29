80beedf44e0dd4234408a1a53b556c99038e65a4
yuhyeon9808 <bwj9808@gmail.com>
2025-08-30 00:12:27 +0900

    메뉴 클릭시 깜빡임 수정

--- src/component/product/ui/ProductCard.tsx
+++ src/component/product/ui/ProductCard.tsx
@@ -6,15 +6,15 @@ import Link from 'next/link';
 export default function ProductCard({ perfume }: { perfume: Perfume }) {
   return (
     <Link href={`/product/detail/${encodeURIComponent(perfume.id)}`}>
-      <div className="w-[234px] sm:w-[250px] rounded-md bg-primary-w pb-9 text-primary-p">
-        <div className="flex flex-col items-center transition-transform duration-200 hover:scale-105">
+      <div className="w-[234px] sm:w-[250px] rounded-md bg-primary-w pb-9 text-primary-p shadow-strong">
+        <div className="flex flex-col items-center ">
           <Image
             src={BASE_URL + encodeURIComponent(perfume.thumbnail_url.trim())}
             alt={perfume.name}
             width={240}
             height={240}
             style={{ width: 'auto' }}
-            className="mt-4 max-h-[240px] object-contain"
+            className="mt-4 h-[240px] object-contain transition-transform duration-200 hover:scale-110"
             placeholder="blur"
             blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWNgYGBgAAAABAABJzQnCgAAAABJRU5ErkJggg=="
             priority
