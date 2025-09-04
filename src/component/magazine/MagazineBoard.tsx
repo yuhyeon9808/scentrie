6413cf1cd66818a0a520f77943b4c2c608302f4f
yuhyeon9808 <bwj9808@gmail.com>
2025-09-04 21:14:50 +0900

    스켈레톤 수정

--- src/component/magazine/MagazineBoard.tsx
+++ src/component/magazine/MagazineBoard.tsx
@@ -10,15 +10,35 @@ import MagazineSearchInput from './ui/MagazineSearchInput';
 import Image from 'next/image';
 import { BASE_URL } from '@/app/core/constants/etc';
 
-function MagazineBoardSkeleton() {
+function CardFrame({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full md:px-28 sm:min-h-[1143px] min-h-[943px]">
+    <div className="relative aspect-[750/428] w-full rounded-md overflow-hidden">
+      {children}
+    </div>
+  );
+}
+
+function MagazineBoardSkeleton() {
+  const SKELETON_COUNT = 8;
+
+  return (
+    <div className="w-full md:px-28">
       <div className="flex flex-col items-center sm:py-28 py-14">
         <span className="pb-14 font-medium text-font-48">MAGAZINE</span>
-        <div className="mb-7 w-full">
-          <div className="skeleton h-[512px] w-full"></div>
+
+        <div className="mb-7 w-full flex justify-center">
+          <ul className="max-w-[1522px] grid lg:grid-cols-2 grid-cols-1 gap-6">
+            {Array.from({ length: SKELETON_COUNT }).map((_, i) => (
+              <li key={i}>
+                <CardFrame>
+                  <div className="absolute inset-0 skeleton" />
+                </CardFrame>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="my-10 self-end"></div>
+
+        <div className="my-10 self-end" />
         <div className="flex">
           <input
             type="text"
