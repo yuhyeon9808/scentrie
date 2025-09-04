59ff0956752e209c5fd08ccd0adbc7adf91963f9
yuhyeon9808 <bwj9808@gmail.com>
2025-09-04 20:34:47 +0900

    매거진 params 타입 수정

--- src/app/magazine/page.tsx
+++ src/app/magazine/page.tsx
@@ -1,5 +1,11 @@
 import MagazineBoard from '@/component/magazine/MagazineBoard';
 
-export default function MagazineBoardPag() {
-  return <MagazineBoard />;
+export default async function MagazineBoardPage({
+  params,
+}: {
+  params: Promise<{ page: string }>;
+}) {
+  const { page } = await params;
+
+  return <MagazineBoard page={page} />;
 }
